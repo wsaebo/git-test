56da1a32fe9870f1dec311cb3c4ab06c1fe13e86
wsaebo <wsaebo@gmail.com>
2024-01-25 10:02:05 +0100

    Nye variabler

--- variabler.py
+++ variabler.py
@@ -1,1 +1,12 @@
-print("Hei Student")
+print("Hei Student") #Printer ut Hei Student
+navn = input() #Legger inn data fra input inn i navn
+print ("Hei " + navn) #Printer ut navn
+tall1=2 #Setter tall 1 = 2
+tall2=4 #Setter tall 2 = 4 
+differanse=tall2-tall1 #Legger inn differanse mellom tall 2 og tall 1 inn i differanse
+print("Differanse" , differanse) #Printer ut differnase 
+nyttnavn=input() #Lager nytt navn 
+sammen = navn+nyttnavn #Legger navn og nyttnavn inn i sammen 
+print (sammen) #Printer sammen slik variabelen er 
+sammen = navn + " og " + nyttnavn #Setter Sammen til en ny verdi med mellomrom
+print (sammen) #Printer sammen som ny variabel. 
