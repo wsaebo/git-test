010db29770b72f623030e3b257fa31262cd45c5b
wsaebo <wsaebo@gmail.com>
2024-01-25 13:36:45 +0100

    Forandrer brusprogrammet slik det står i oppgaven

--- beslutninger.py
+++ beslutninger.py
@@ -1,12 +1,13 @@
-print ("Vil du ha en brus?" )
-brus = input()
+#Dette programmet leser inn og spør en person om man vil ha en brus. 
+print ("Vil du ha en brus?" ) #Printer ut spørsmålet vil du ha en brus ? 
+brus = input() #Tar i mot input fra brukeren
 brus = str.upper(brus) #Konverterer strengen til uppcase slik at om brukeren taster nei, NEI, JA, jA osv så vil det fungere uansett :) 
-if (brus=="JA"): 
+if (brus=="JA"): #Om brus er lik ja så får du en brus 
     print("Her har du en brus")
-elif (brus=="NEI") : 
-    print ("Ingen brus i dag")
+elif (brus=="NEI") : #Om brus er lik nei så får du ikke brus og printer ut den er god 
+    print ("Den er god")
 
 else: 
-    print("Det forstod jeg ikke")
+    print("Det forstod jeg ikke") #printer ut det forstod jeg ikke om input er noe annet enn ja eller nei. 
 
 
